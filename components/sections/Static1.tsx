77a243de44112a76330e934f7733770b2421d66f
yossi6206 <-@example.com>
2026-05-02 20:08:40 +0300

    תיאור השינוי

--- components/sections/Static1.tsx
+++ components/sections/Static1.tsx
@@ -10,50 +10,50 @@ export default function Static1() {
 				<div className="container">
 					<div className="inner">
 						<div className="row align-items-center justify-content-between">
-							<div className="col-lg-auto col-md-6">
-								<div className="counter-item-cover counter-item">
-									<div className="content text-center mx-auto d-flex align-items-center">
-										<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={12} className="odometer ds-1 text-dark fw-semibold" /></span>
-										<div className="text-start ms-2">
-											<p className="fs-5 mb-0 text-300">שנות</p>
-											<p className="fs-5 mb-0 fw-bold">ניסיון</p>
-										</div>
+						<div className="col-lg-auto col-md-6">
+							<div className="counter-item-cover counter-item">
+								<div className="content text-center mx-auto d-flex align-items-center">
+									<div className="text-end me-2">
+										<p className="fs-5 mb-0 text-300">לקוחות</p>
+										<p className="fs-5 mb-0 fw-bold">מרוצים</p>
 									</div>
+									<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={680} className="odometer ds-1 text-dark fw-semibold" /></span>
 								</div>
 							</div>
-							<div className="col-lg-auto col-md-6">
-								<div className="counter-item-cover counter-item">
-									<div className="content text-center mx-auto d-flex align-items-center">
-										<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={250} className="odometer ds-1 text-dark fw-semibold" /></span>
-										<div className="text-start ms-2">
-											<p className="fs-5 mb-0 text-300">פרויקטים</p>
-											<p className="fs-5 mb-0 fw-bold">הושלמו</p>
-										</div>
+						</div>
+						<div className="col-lg-auto col-md-6">
+							<div className="counter-item-cover counter-item">
+								<div className="content text-center mx-auto d-flex align-items-center">
+									<div className="text-end me-2">
+										<p className="fs-5 mb-0 text-300">פרויקטים</p>
+										<p className="fs-5 mb-0 fw-bold">הושלמו</p>
 									</div>
+									<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={250} className="odometer ds-1 text-dark fw-semibold" /></span>
 								</div>
 							</div>
-							<div className="col-lg-auto col-md-6">
-								<div className="counter-item-cover counter-item">
-									<div className="content text-center mx-auto d-flex align-items-center">
-										<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={680} className="odometer ds-1 text-dark fw-semibold" /></span>
-										<div className="text-start ms-2">
-											<p className="fs-5 mb-0 text-300">לקוחות</p>
-											<p className="fs-5 mb-0 fw-bold">מרוצים</p>
-										</div>
+						</div>
+						<div className="col-lg-auto col-md-6">
+							<div className="counter-item-cover counter-item">
+								<div className="content text-center mx-auto d-flex align-items-center">
+									<div className="text-end me-2">
+										<p className="fs-5 mb-0 text-300">פרסים</p>
+										<p className="fs-5 mb-0 fw-bold">זכיות</p>
 									</div>
+									<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={18} className="odometer ds-1 text-dark fw-semibold" /></span>
 								</div>
 							</div>
-							<div className="col-lg-auto col-md-6">
-								<div className="counter-item-cover counter-item">
-									<div className="content text-center mx-auto d-flex align-items-center">
-										<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={18} className="odometer ds-1 text-dark fw-semibold" /></span>
-										<div className="text-start ms-2">
-											<p className="fs-5 mb-0 text-300">פרסים</p>
-											<p className="fs-5 mb-0 fw-bold">זכיות</p>
-										</div>
+						</div>
+						<div className="col-lg-auto col-md-6">
+							<div className="counter-item-cover counter-item">
+								<div className="content text-center mx-auto d-flex align-items-center">
+									<div className="text-end me-2">
+										<p className="fs-5 mb-0 text-300">שנות</p>
+										<p className="fs-5 mb-0 fw-bold">ניסיון</p>
 									</div>
+									<span className="ds-3 count text-primary-1 fw-medium my-0">+<CountUp enableScrollSpy={true} end={12} className="odometer ds-1 text-dark fw-semibold" /></span>
 								</div>
 							</div>
+						</div>
 						</div>
 					</div>
 				</div>
